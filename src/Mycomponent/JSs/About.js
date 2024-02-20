087afb6e7163ab7de8527c29157653f3b5610d5c
ARYAN VERMA <aryanver6@gmail.com>
2024-02-20 19:13:17 +0530

    modified

--- src/Mycomponent/JSs/About.js
+++ src/Mycomponent/JSs/About.js
@@ -4,7 +4,7 @@ import linedesign from "../Images/line-design.png"
 
 export default function About() {
   return (
-    <div>
+    <>
       <Hero />
       <div className="bg-about">
         <div className="df col">
@@ -19,16 +19,18 @@ export default function About() {
           </p>
           <p>Step into our online sanctuary, where each click opens a door to a world where elegance and craftsmanship converge. Whether you seek a timeless piece for a special occasion or desire to indulge in the everyday luxury of gold and diamond, our curated collections are designed to cater to every discerning taste. From classic designs that stand the test of time to contemporary creations that redefine sophistication, our diverse range ensures there's something for everyone.
           </p>
-          <div className="phn-hide" data-aos="fade-in">
-            <p>At Jaiprakash Ornament House, we understand that gold is not just a metal, it's a reflection of your unique style and personality. Our artisans, masters in their craft, pour passion and skill into every creation, ensuring that each piece is a testament to the dedication we have for our art. The result is a collection that exudes a distinctive charm, where tradition meets modernity, and every detail is meticulously considered.
-              As you explore our virtual aisles, you'll find an array of gold jewellery that transcends trends, making a statement that is as enduring as the metal itself. From intricately designed necklaces that grace the neckline with timeless grace to delicately crafted bracelets that adorn the wrist with subtle elegance, our pieces are more than accessories, they are heirlooms in the making.
-            </p>
-            <p>Transparency and trust form the foundation of our relationship with our patrons. Jaiprakash Ornament House takes pride in sourcing only the highest quality gold, adhering to stringent ethical standards. Our commitment to authenticity ensures that each piece you acquire from us is not just a work of art but a genuine investment in craftsmanship and value.
-              Join us in celebrating the elegance of gold as well as silver and diamond at Jaiprakash Ornament House. Indulge your senses, ignite your passion for fine jewellery, and discover the embodiment of luxury in every piece. As you navigate through our online realm, we invite you to make each selection not just a purchase but a reflection of your unique style and a celebration of life's precious moments. Welcome to a world where gold isn't just a metal, it's an expression of refined taste, a symbol of enduring beauty, and an embodiment of timeless elegance.</p>
-          </div>
+          <p>At Jaiprakash Ornament House, we understand that gold is not just a metal, it's a reflection of your unique style and personality. Our artisans, masters in their craft, pour passion and skill into every creation, ensuring that each piece is a testament to the dedication we have for our art. The result is a collection that exudes a distinctive charm, where tradition meets modernity, and every detail is meticulously considered.
+            As you explore our virtual aisles, you'll find an array of gold jewellery that transcends trends, making a statement that is as enduring as the metal itself. From intricately designed necklaces that grace the neckline with timeless grace to delicately crafted bracelets that adorn the wrist with subtle elegance, our pieces are more than accessories, they are heirlooms in the making.
+          </p>
+          <p>Transparency and trust form the foundation of our relationship with our patrons. Jaiprakash Ornament House takes pride in sourcing only the highest quality gold, adhering to stringent ethical standards. Our commitment to authenticity ensures that each piece you acquire from us is not just a work of art but a genuine investment in craftsmanship and value.
+            Join us in celebrating the elegance of gold as well as silver and diamond at Jaiprakash Ornament House. Indulge your senses, ignite your passion for fine jewellery, and discover the embodiment of luxury in every piece. As you navigate through our online realm, we invite you to make each selection not just a purchase but a reflection of your unique style and a celebration of life's precious moments. Welcome to a world where gold isn't just a metal, it's an expression of refined taste, a symbol of enduring beauty, and an embodiment of timeless elegance.</p>
+        </div>
+        <div className="df col">
+          <img src={linedesign} alt="" id="line" />
+          <span className="heads">PURE | HONEST | ELOQUENT</span>
+          <span className="contents">Jewellery as beautiful as You</span>
         </div>
       </div>
-
-    </div>
+    </>
   );
 }
